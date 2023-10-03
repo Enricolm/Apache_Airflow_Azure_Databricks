2e7d7f5801a12b320fb52b5f6666ebe5079d5c0f
Enricolm <Enricolm>
2023-10-03 13:32:24 +0000

    arrumando_a_classe

--- Spark_Extraction.py
+++ Spark_Extraction.py
@@ -7,7 +7,7 @@ from pyspark.sql.types import StructType, StructField, StringType
 
 
 
-dbutils.fs.mkdirs("dbfs:/FileStore/tables/Tokens/")
+dbutils.fs.mkdirs("dbfs:/FileStore/tables/Extraction_Data_Sprin/Tokens/")
 
 
 # COMMAND ----------
@@ -17,6 +17,89 @@ display(dbutils.fs.head("dbfs:/FileStore/tables/Tokens/access_token.json"))
 # COMMAND ----------
 
 display(dbutils.fs.head("dbfs:/FileStore/tables/Tokens/payload.json"))
+
+# COMMAND ----------
+
+class extracao_valores():
+    def __init__ (self):
+        self.key = 'fnks2xz3k6k8x22vunu399u3'
+        self.secret = '5VDfQd8qKCqJtahsRze8PrxChpXPK3NUVnB7JAWXMfByBvSXSN6rmQZV2Btr3Uhw'
+        self.redirect_uri = 'https://www.sprinklr.com/pt-br/'
+        self.auth_token='637d1e15f29eb122a26b4921'
+        self.code = '63867cf7b1afe75617a6e1b0'
+        super().__init__()
+
+
+
+    def credentials (self):
+        with open("/dbfs/FileStore/tables/Tokens/access_token.json", 'r') as j:
+            i = json.load(j)    
+            ref_token = (i['access_token'])
+
+        headers = {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + ref_token,
+                    'Key': 'fnks2xz3k6k8x22vunu399u3'
+                }    
+
+        with open('/dbfs/FileStore/tables/Tokens/payload.json', 'r+') as j:
+            payload = json.load(j)
+        self.headers = headers
+        self.payload = payload
+
+    def extraction_values_and_merge(self):
+        i = 0
+        data = []
+        payload = self.payload
+        headers = self.headers
+        while True:
+            print(i)
+            payload['page'] = i
+            request_url = 'https://api2.sprinklr.com/prod2/api/v2/reports/query'
+            request = requests.post(request_url, json=payload, headers=headers, verify=False)
+            response = request.text
+            obj_json2 = json.loads(response)
+            data.append(obj_json2['data']['rows'])
+            i = i + 1
+            if  120 != len(obj_json2['data']['rows']):
+                break
+        self.data = data
+    
+    def create_dataframe(self):
+
+        data_list = self.data
+
+        schema = StructType([
+            StructField("Data", StringType(), True),
+            StructField("Sentimento", StringType(), True),
+            StructField("Valor", StringType(), True)
+            ])
+        
+
+        data = spark.createDataFrame([], schema=schema)
+        for i in range (len(data_list)):
+            
+            for j in (data_list[i]):
+                data = data.union(spark.createDataFrame([j], schema=schema))
+        self.data = data
+    
+    def criando_pasta(self):
+        
+        dbutils.fs.mkdirs("dbfs:/FileStore/tables/Extraction_Data_Sprin/Data/")
+
+
+    def save_dataframe(self):
+
+        self.data.write.format("parquet")\
+            .mode("overwrite")\
+            .save()
+        
+
+
+
+
+    
+
 
 # COMMAND ----------
 
